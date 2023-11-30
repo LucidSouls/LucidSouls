fd2c26c0a5857a21eb721a410300bd819f6c7f6a
Kora <121691222+KoraLSs@users.noreply.github.com>
2023-11-29 23:37:08 -0500

    Update LucidSoulsPawn.cpp

--- Source/LucidSouls/PawnsCPP/LucidSoulsPawn.cpp
+++ Source/LucidSouls/PawnsCPP/LucidSoulsPawn.cpp
@@ -35,23 +35,56 @@ ALucidSoulsPawn::ALucidSoulsPawn(const FObjectInitializer& ObjectInitializer)
         Camera->SetupAttachment(CameraArm, USpringArmComponent::SocketName);
 }
 
-// Called when the game starts or when spawned
+
 void ALucidSoulsPawn::BeginPlay()
 {
 	Super::BeginPlay();
 
 }
 
-// Called every frame
+
 void ALucidSoulsPawn::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
 }
 
-void ALucidSoulsPawn::MoveTowards(FVector AbsoluteMovementDirection)
-{
-    //MovementComponent->AddInputVector(AbsoluteMovementDirection);
-    //AddMovementInput(AbsoluteMovementDirection);
-    UE_LOG(LogTemp, Warning, TEXT("MoveTowards is called!"));
+void CharacterAnim::UpdateLocomotionState(ECharacterState NewState) {
+    if (CurrentState != NewState) {
+        CurrentState = NewState;
+        switch (NewState) {
+        case ECharacterState::Running:
+            PlayRunningAnimation();
+            break;
+        case ECharacterState::Sprinting:
+            PlaySprintingAnimation();
+            break;
+        case ECharacterState::Jumping:
+            PlayJumpAnimation();
+            break;
+            // Other locomotion states and their respective animations
+        }
+    }
+}
+
+// This code snippet demonstrates a custom animation blending system for fluid transitions.
+
+void CharacterAnim::BlendAnimations(UAnimMontage* BaseMontage, UAnimMontage* OverlayMontage) {
+    if (BaseMontage && OverlayMontage) {
+        UAnimInstance* AnimInstance = GetMesh()->GetAnimInstance();
+        if (AnimInstance) {
+            float BlendDuration = 0.2f; // Set blending duration
+            AnimInstance->Montage_Play(BaseMontage);
+            AnimInstance->Montage_Play(OverlayMontage, BlendDuration);
+            // Logic for handling blending between animations
+        }
+    }
+}
+
+void CharacterAnim::JumpToAnimationSection(FName SectionName, UAnimMontage* Montage) {
+    UAnimInstance* AnimInstance = GetMesh()->GetAnimInstance();
+    if (AnimInstance && Montage) {
+        AnimInstance->Montage_Play(Montage);
+        AnimInstance->Montage_JumpToSection(SectionName, EAnimPlayMode::Stop);
+        // Improve the logic for more precise section jumps
+    }
 }
